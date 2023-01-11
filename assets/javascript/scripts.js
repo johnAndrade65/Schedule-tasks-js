d68b97e0a6da5ecbee54d7ffdd226cec51f24459
john65 <joaovini2667@gmail.com>
2023-01-11 19:51:32 -0300

    fixed-bugs

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,4 +1,4 @@
-//Variaveis DOM
+//VARIAVEIS DOM
 let tasksShow = document.getElementById('task-lists');
 let tittleTask = document.getElementById('title');
 let descriptionTask = document.getElementById('description');
@@ -7,22 +7,22 @@ let btnSave = document.getElementById('save-task');
 let btnMoreTask = document.querySelector('#more-task');
 let displayTaskAdd = document.getElementById('tasks-add');
 
-//Botão aciona tela de adicionar tasks
+//BOTÃO CHAMA TELA DE ADICIONAR TAREFAS
 btnMoreTask.addEventListener('click', () => {
     displayTaskAdd.style.display = 'flex'
     btnMoreTask.style.display = 'none'
 });
 
-//Botão salvar Tarefa e fechar tela de adicionar task
+//BOTÃO SALVA A TAREFA E FECHA A TELA DE ADICIONAR TAREFAS
 btnSave.addEventListener("click", () => {
     displayTaskAdd.style.display = 'none'
     btnMoreTask.style.display = 'flex'
 
-    //Chama a function para renderizar as tarefas
+    //CHAMA A FUNÇÃO PARA RENDERIZAR AS TAREFAS
     renderTasks();
 });
 
-//Pegar valor do input e textearea, transformar em elemento HTML e renderizar
+//PEGA O VALOR DO INPUT E TEXTAREA, TRASNFERE A ELEMENTOS "<li>" HTML E ENVIA PARA SER RENDERIZADA
 function renderTasks() {
     const newTaskTitle = tittleTask.value;
     tittleTask.value = ""; 
@@ -30,7 +30,7 @@ function renderTasks() {
     const newTaskDescription = descriptionTask.value;
     descriptionTask.value = "";
     
-    //Renderizar tarefas
+    //RENDERIZAR AS TAREFAS
     tasksShow.insertAdjacentHTML("beforeend", 
     `<ul class="tasks-show-list">
         <li class="li-title"> ${newTaskTitle} - Title</li>
